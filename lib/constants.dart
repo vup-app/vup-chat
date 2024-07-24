76bccaa96203182c7b668425e0e084aa980180a4
covalent <mail@lukehmcc.com>
2024-07-24 12:28:50 -0400

    bump version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-const String version = "v0.5.17";
+const String version = "v0.5.19";
 
 const defaultAccentColor = Color(0xFF1ED660);
 const lightBackgroundColor = Color(0xfffafafa);
