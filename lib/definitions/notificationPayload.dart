3fa712df10d5206e47a0198b33e25bfe41c3fad4
covalent <mail@lukehmcc.com>
2024-07-26 17:53:50 -0400

    feat(android): allow responding to messages through notification (forgot the payload object)

---
+++ lib/definitions/notificationPayload.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'notificationPayload.g.dart';
+
+@JsonSerializable()
+class NotificationPayload {
+  final String? did;
+  final String chatID;
+
+  NotificationPayload({
+    required this.did,
+    required this.chatID,
+  });
+
+  // Factory constructor to create an S5Embed instance from JSON
+  factory NotificationPayload.fromJson(Map<String, dynamic> json) =>
+      _$NotificationPayloadFromJson(json);
+
+  // Method to convert an S5Embed instance to JSON
+  Map<String, dynamic> toJson() => _$NotificationPayloadToJson(this);
+}
---
+++ lib/definitions/notificationPayload.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notificationPayload.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NotificationPayload _$NotificationPayloadFromJson(Map<String, dynamic> json) =>
+    NotificationPayload(
+      did: json['did'] as String?,
+      chatID: json['chatID'] as String,
+    );
+
+Map<String, dynamic> _$NotificationPayloadToJson(
+        NotificationPayload instance) =>
+    <String, dynamic>{
+      'did': instance.did,
+      'chatID': instance.chatID,
+    };
